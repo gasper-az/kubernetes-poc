294ce014dd04dee5196c708819e75676d9a5fdfc
Gaspar Acevedo <acevedo.zain.gaspar@gmail.com>
2025-08-14 16:36:25 -0300

    [infra-modules] Base deployment with subnets.

--- infra-modules/deployable_units/base/vnet-subnets.tf
+++ infra-modules/deployable_units/base/vnet-subnets.tf
@@ -6,3 +6,24 @@ module "vnet" {
   address_space       = var.vnet_specs.addres_space
   tags                = local.tags
 }
+
+module "kubernetes_subnets" {
+  source               = "../../modules/subnets"
+  name                 = var.kubernetes_subnet_specs.name
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  virtual_network_name = module.vnet.name
+  address_prefixes     = var.kubernetes_subnet_specs.address_prefixes
+}
+
+variable "kubernetes_subnet_specs" {
+  description = <<EOT
+    kubernetes_subnet_specs = {
+      name:             String. The subnet's name.
+      address_prefixes: String. The subnet's CIDR.
+    }
+  EOT
+  type = object({
+    name             = string
+    address_prefixes = string
+  })
+}
